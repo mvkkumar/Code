5c2427304591ef89203c46a3c3a2d90b5f124368
mvkkumar <venkatakiran.matturi@gmail.com>
2015-10-10 13:05:21 -0700

    Linked List Loop

---
+++ linkedListLoops.c
@@ -0,0 +1,112 @@
+//
+//  linkedListLoops.c
+//  
+//
+//  Created by Kiran Matturi on 10/6/15.
+//
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct nod
+{
+    int value;
+    struct nod *next;
+}node;
+
+void insertNode( node* head, int value)
+{
+    node * newNode = (node*)malloc(sizeof(node));
+    newNode->value = value;
+    newNode->next = NULL;
+    node * tempNode = head;
+    while (tempNode->next != NULL)
+    {
+        tempNode  = tempNode->next;
+    }
+    tempNode->next = newNode;
+}
+
+void createLoop(node * head, int loopValue)
+{
+    node * tail = head;
+    node * currentNode = head;
+    
+    while ( tail->next != NULL )
+    {
+        tail = tail->next;
+    }
+    printf("looped at %i \n",tail->value);
+    
+    while(  currentNode->value != loopValue )
+    {
+        currentNode = currentNode->next;
+    }
+    if( currentNode->value == loopValue )
+    {
+        tail->next = currentNode;
+    }
+        printf("looped to %i \n",currentNode->value);
+    
+}
+node * findLoopHead( node* head)
+{
+    node* tortoise = head->next;
+    node* hare = head->next->next;
+    
+    while ( (tortoise->next != NULL) && (hare->next != NULL) )
+    {
+        if ( tortoise->value == hare->value )
+        {
+            break;
+        }
+        else
+        {
+            tortoise = tortoise->next;
+            hare = hare->next->next;
+        }
+    }
+    printf("Successfully Looped at %i \n",tortoise->value);
+    /* Now we got the member of the loop, lets find the length */
+    int count = 0;
+    
+    tortoise = tortoise->next;
+    count++;
+    while (tortoise->value != hare->value)
+    {
+        tortoise = tortoise->next;
+        count++;
+    }
+    printf("number of elements in the loop is %i",count);
+    
+    /* Now find the start of the Loop */
+    node * tempNode = head;
+    
+    while (tempNode->value != tortoise->value)
+    {
+        tempNode = tempNode->next;
+        tortoise = tortoise->next;
+    }
+    
+    return tempNode;
+}
+int main()
+{
+ 
+    node * head = NULL;
+    head = (node*)malloc(sizeof(node));
+    head->value = 1;
+    head->next = NULL;
+    
+    node * loopHead = NULL;
+//    insertNode(head,2);
+    for ( int i = 0; i<15; i++)
+        insertNode(head,(i+2));
+
+    createLoop(head,4);
+    
+    loopHead = findLoopHead(head);
+    printf("Successfully Looped at %i \n",loopHead->value);
+    return 0;
+}
--- linksIntersection.c
+++ linksIntersection.c
@@ -4,7 +4,48 @@
 //
 //  Created by Kiran Matturi on 10/5/15.
 //
+//  Given Two Arrays: find the values that occur in both the arrays
+//
 //
 
 #include <stdio.h>
 
+
+//int* getCommonArray( int a[], int b[], int * count ,int array1Len,int array2Len)
+//{
+////    int array1Len = sizeof(a)/sizeof(a[0]);
+////    int array2Len = sizeof(b)/sizeof(b[0]);
+//
+//    return commonArray;
+//}
+
+
+int main ()
+{
+    int array1[] = {10,11,19,33,25,44};
+    int array2[] = {99,3,65,4,9,56,31,73,11,19};
+    int count = 0;
+    
+    int array1Len = sizeof(array1)/sizeof(array1[0]);
+    int array2Len = sizeof(array2)/sizeof(array2[0]);
+    int len = 5;
+    //length = array1Len>array2Len?array2Len:array1Len;
+    
+    printf("%i",len);
+    int commonArray[len] = {0};
+    count = 0;
+    for ( int i = 0 ; i<array1Len; i++)
+    {
+        
+        int key = array1[i];
+        for ( int j = 0; j< array2Len;j++)
+        {
+            if ( key == array2[j])
+            {
+                commonArray[count] = key;
+                count++;
+            }
+        }
+    }
+    return 0;
+}
